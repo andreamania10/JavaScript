c987c3f4a363e50f2e5b34fc12dad20731ab1995
Andrea <andreamania10@gmail.com>
2024-03-11 19:47:24 +0100

    subida

--- EJERCICIOS/EjerciciosconHTML/Marzo/Marzo/Semana 2/Ejercicio12ahorcado.js
+++ EJERCICIOS/EjerciciosconHTML/Marzo/Marzo/Semana 2/Ejercicio12ahorcado.js
@@ -1,12 +1,35 @@
-const palabras = ['mar', 'montaña', 'cielo', 'aire'].
+const replaceAt = (string, character, index) => {
+    return string.substring(0, index) + character + string.substring(index + character.length);
+}
+
+let hemosGanado = false;
+
+const palabras = ['mar', 'montaña', 'cielo', 'aire']
 const posicionaleatoria = Math.floor(Math.random() * palabras.length)
 const palabraSecreta = palabras[posicionaleatoria]
 
 let palabraConGuiones = palabraSecreta.replace(/./g, "_ ");
-const pregunta = prompt('Introduce una letra, por favor');
 
-for (let i = 0; i < posicionaleatoria.length; i++) {
-    if (pregunta === posicionaleatoria[i]) {
+while (!hemosGanado) {
+    alert(palabraConGuiones)
 
+    const letra = prompt('Introduce una letra, por favor');
+    let letraEncontrada = false;
+
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (letra === palabraSecreta[i]) {
+            palabraConGuiones = replaceAt(palabraConGuiones, letra, i * 2);
+            letraEncontrada = true;
+        }
     }
+    if (!letraEncontrada) {
+        alert("Has fallado")
+    }
+    if (palabraConGuiones.indexOf("_") == -1) {
+        alert("Has ganado");
+        hemosGanado = true;
+    }
+
 }
+
+

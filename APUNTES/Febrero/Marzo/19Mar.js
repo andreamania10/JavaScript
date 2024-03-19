ca6bb69f9a3b0f28e668621e79a648d30d6f89c6
Andrea <andreamania10@gmail.com>
2024-03-19 19:44:52 +0100

    apuntes

--- APUNTES/Febrero/Marzo/19Mar.js
+++ APUNTES/Febrero/Marzo/19Mar.js
@@ -1,2 +1,10 @@
 Propiedad style: 
 Le das style a  un elemento.Se pone normalmente obj.style(y lo que quieres, como puede ser color): obj.style.color = 'ff0000'
+
+    //AÑADIR CLASE, BORRAR CLASE, ALTERNAR CLASE, EVALUAR SI TIENE UNA CLASE
+    - Añadir clase: obj.classList.add('nombreClase');
+- Cambiar clase: obj.classList.toggle('nombreClase');
+- Eliminar clase: obj.classList.remove('nombreClase');
+- Evaluar si tiene una clase: if (obj.classList.contains('thatClass')) {
+    // do some stuff
+}

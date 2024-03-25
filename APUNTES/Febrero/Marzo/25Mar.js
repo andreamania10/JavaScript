2c562123cbd339320207584a7aa7e67eb731fcf0
Andrea <andreamania10@gmail.com>
2024-03-25 19:41:43 +0100

    25/03/2024

---
+++ APUNTES/Febrero/Marzo/25Mar.js
@@ -0,0 +1,3 @@
+confirm(cuando en vez de un prompt te sale una pestaña de OK), como un alert.
+    if(confirm('play again?')) {
+    reset()
---
+++ EJERCICIOS/EjerciciosconHTML/Marzo/Semana 5/Archivos JS/Ejerciciotresenraya.js
@@ -0,0 +1,39 @@
+let turno = 0
+const tablero = []
+
+const botonclick = (e, position) => {
+    turno++;
+    const btn = e.target;
+    const color = turno % 2 ? 'salmon' : 'green'
+    btn.style.backgroundColor = color;
+    tablero[position] = color;
+    if (haGanado()) {
+        alert('Enhorabuena player ' + color)
+    }
+}
+
+const haGanado = () => {
+    if (tablero[0] == tablero[1] && tablero[0] == tablero[2] && tablero[0]) {
+        return true;
+    } else if (tablero[3] == tablero[4] && tablero[3] == tablero[5] && tablero[3]) {
+        return true;
+    } else if (tablero[6] == tablero[7] && tablero[6] == tablero[8] && tablero[6]) {
+        return true;
+    } else if (tablero[0] == tablero[3] && tablero[0] == tablero[6] && tablero[0]) {
+        return true;
+    } else if (tablero[1] == tablero[4] && tablero[1] == tablero[7] && tablero[1]) {
+        return true;
+    } else if (tablero[2] == tablero[5] && tablero[2] == tablero[8] && tablero[2]) {
+        return true;
+    } else if (tablero[0] == tablero[4] && tablero[0] == tablero[8] && tablero[0]) {
+        return true;
+    } else if (tablero[2] == tablero[4] && tablero[2] == tablero[6] && tablero[2]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+document.querySelectorAll("button").forEach((obj, i) => {
+    obj.addEventListener('click', (e) => botonclick(e, i));
+})
